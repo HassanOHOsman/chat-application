47fd444894c52fd9d86a9f05ec3edca60f5f0de0
HassanOHOsman <hassan.oh.osman@gmail.com>
2026-01-15 11:13:39 +0000

    add constructor, getters and setters to Message class

--- backend/server.java
+++ backend/server.java
@@ -1,7 +1,39 @@
 
-public class message {
-    String user;
-    String content;
-    long timestamp;
+public class Message {
+
+    private String user;
+    private String content;
+    private long timestamp;
+
+    public Message(String user, String content, long timestamp) {
+        this.user = user;
+        this.content = content;
+        this.timestamp = timestamp;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+
     
 }
