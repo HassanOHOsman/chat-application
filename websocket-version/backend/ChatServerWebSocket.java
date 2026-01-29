5f615215c86394d83156398a17f883cddb7aab19
HassanOHOsman <hassan.oh.osman@gmail.com>
2026-01-29 20:03:45 +0000

    added all the libraries that will be used in my backend entry-point logic at the top

--- websocket-version/backend/ChatServerWebSocket.java
+++ websocket-version/backend/ChatServerWebSocket.java
@@ -1,3 +1,14 @@
+import org.java_websocket.server.WebSocketServer;
+import org.java_websocket.WebSocket;
+import org.java_websocket.handshake.ClientHandshake;
+
+import java.net.InetSocketAddress;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+
+
 public class ChatServerWebSocket {
 
     public static void main(String[] args) {
